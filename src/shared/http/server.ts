0a1b4a9114a90b05066278679451867d90797c84
Gabriel Santiago <tocansadonis@gmail.com>
2025-02-25 21:08:27 -0300

    feat: user migration

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
@@ -15,6 +16,7 @@ AppDataSource.initialize()
     app.use(express.json());
 
     app.use(routes);
+    app.use(errors());
     app.use(ErrorHandleMiddleware.haddleError);
 
     console.log('Connected to the database!');
